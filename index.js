088b8fcf934ed5b7b96fccb8510d05458b6f35c7
BlazingFire007 <elijahrich02@icloud.com>
2022-10-30 17:57:47 -0500

    init2

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import { Board, search } from 'exesjs';
 
-const board = new Board({
-  width: 7,
-  height: 7,
-  winLength: 4,
-  turn: 1,
-});
-
 function randomMove(b) {
   const moves = b.moves;
   const move = moves[Math.floor(Math.random() * moves.length)];
   b.move(move);
 }
 
-while (!board.isOver) {
-  randomMove(board);
-  board.move(search(board, 5));
+for (let i = 0; i < 10; i++) {
+  console.time('game');
+  const board = new Board({
+    width: 6,
+    height: 6,
+    winLength: 4,
+    turn: 1,
+  });
+  while (!board.isOver) {
+    board.move(search(board, 5));
+    board.move(search(board, 5));
+  }
+  console.timeEnd('game');
 }
-board.print();
-console.log(board.winner === 1 ? 'Random won!' : 'AI won!');
